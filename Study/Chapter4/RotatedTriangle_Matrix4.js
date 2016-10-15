bc60da71d42cc7ae0437f6bb74cd83541fc01986
jongber <jongber@gmail.com>
2016-10-15 16:49:53 +0900

    tick 애니메이션 추가

---
+++ Study/Chapter4/RotatedTriangle_Matrix4.js
@@ -0,0 +1,75 @@
+
+
+var VSHADER_SOURCE = 
+        'attribute vec4 a_Position;\n' +
+        'uniform mat4 u_xformMatrix;\n' +
+        'void main() {\n' +
+            'gl_Position = a_Position * u_xformMatrix;\n'+
+        '}';
+
+var FSHADER_SOURCE =
+    'void main() {' +
+        'gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);'+
+    '}';
+
+var Tx = 0.5, Ty = 0.5, Tz = 0.0;
+var ANGLE = 90.0;
+
+function main(){
+    var canvas = document.getElementById("webgl");
+
+    var gl = getWebGLContext(canvas);
+    if (!gl) {
+        return;
+    }
+
+    if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
+        console.log('Failed to initShader shader');
+        return;
+    }
+
+    var n = initVertexBuffers(gl);
+
+    var u_xformMatrix = gl.getUniformLocation(gl.program, 'u_xformMatrix');
+    if (u_xformMatrix < 0) {
+        console.log('Failed to u_Translation');
+        return;
+    }
+
+    var radian = Math.PI * ANGLE/ 180.0;
+
+    var xformMatrix = new Matrix4();
+    xformMatrix.setRotate(ANGLE, 0, 0, 1);
+
+    gl.uniformMatrix4fv(u_xformMatrix, false, xformMatrix.elements);
+
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.drawArrays(gl.TRIANGLES, 0, n);
+}
+
+function initVertexBuffers(gl) {
+    var vertices = new Float32Array([0.0, 0.5, -0.5, -0.5, 0.5, -0.5]);
+    var n = 3;
+
+    var vertexBuffer = gl.createBuffer();
+    if (vertexBuffer < 0) {
+        console.log("fail to create vertex buffer");
+        return;
+    }
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+    var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+    if (a_Position < 0) {
+                console.log('Failed to get the storage location of a_Position');
+        return;
+    }
+
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(a_Position);
+
+    return n;
+}
